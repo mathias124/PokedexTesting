6699de561828047ff967a10f3f082a99de23f5a6
s224292 <s224292@student.dtu.dk>
2023-10-09 18:32:35 +0200

    garfield baby!

--- app/src/main/java/com/example/pokedextesting/FilterButton.kt
+++ app/src/main/java/com/example/pokedextesting/FilterButton.kt
@@ -1,11 +1,14 @@
 package com.example.pokedextesting
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class FilterButton : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.filterbutton_activity)
+        val garfield = findViewById<ImageView>(R.id.garfield)
     }
 }
