d9376b28d66f3391d70c8beb3f6ff4e6e8980d42
s224292 <s224292@student.dtu.dk>
2023-10-10 14:34:54 +0200

    ImageButton Testing

---
+++ app/src/main/java/com/example/pokedextesting/Icon.kt
@@ -0,0 +1,15 @@
+package com.example.pokedextesting
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class IconB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_icon)
+
+        }
+    }
--- app/src/main/java/com/example/pokedextesting/NewActivity.kt
+++ app/src/main/java/com/example/pokedextesting/NewActivity.kt
@@ -1,11 +1,22 @@
 package com.example.pokedextesting
 
+import android.content.Intent
+import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_activity)
+
+
+        val buttonImage = findViewById<ImageButton>(R.id.imageButton)
+        buttonImage.setOnClickListener {
+            val intent = Intent(this, IconB::class.java)
+            startActivity(intent)
+        }
     }
 }
